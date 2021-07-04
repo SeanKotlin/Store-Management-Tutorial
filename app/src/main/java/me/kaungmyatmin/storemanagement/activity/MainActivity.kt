5558ae2dfa404de467419d335714c54153f2de95
sklinn <soe.khaing.linn86@gmail.com>
2021-07-04 19:11:11 +0630

    initial

---
+++ app/src/main/java/me/kaungmyatmin/storemanagement/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.kaungmyatmin.storemanagement.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.kaungmyatmin.storemanagement.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
